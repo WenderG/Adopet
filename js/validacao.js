159f74b333f5ff48adcff3b1a6a0d2caf265144f
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2022-07-18 21:00:09 -0300

    trabalhando no cadastro.css

---
+++ js/validacao.js
@@ -0,0 +1,30 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagemDeErro = {
+    senha: {
+        valueMissing: "O campo de senha não pode estar vazio.",
+        patternMisMatch: "A senha deve conter ao menos 6 caracteres, um número e uma letra maíscula",
+
+        }
+    }
